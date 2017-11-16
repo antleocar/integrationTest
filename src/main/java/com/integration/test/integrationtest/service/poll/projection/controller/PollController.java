d28ad5885df7e666d591b8b30b516a9118d7e67f
Antonio <antonioleoncarrillo@gmail.com>
2017-11-16 11:38:08 +0100

    🦄

--- src/main/java/com/integration/test/integrationtest/service/poll/projection/controller/PollController.java
+++ src/main/java/com/integration/test/integrationtest/service/poll/projection/controller/PollController.java
@@ -1,11 +1,30 @@
 package com.integration.test.integrationtest.service.poll.projection.controller;
 
 
+import com.integration.test.integrationtest.service.poll.projection.repository.VoteRepository;
+import com.integration.test.integrationtest.service.vote.projection.VoteProjection;
+import java.util.List;
+import java.util.UUID;
+import javax.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/poll")
 public class PollController {
+
+  @Autowired
+  private VoteRepository voteRepository;
+
+  @RequestMapping(value = "/{idPoll}/votes", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<VoteProjection> getVotes(@PathParam("id") UUID idPoll) {
+    List<VoteProjection> votes = voteRepository.getVotes(idPoll);
+    return new ResponseEntity(votes, HttpStatus.OK);
+  }
 
 }
--- src/main/java/com/integration/test/integrationtest/service/poll/projection/repository/VoteRepository.java
+++ src/main/java/com/integration/test/integrationtest/service/poll/projection/repository/VoteRepository.java
@@ -4,12 +4,13 @@ package com.integration.test.integrationtest.service.poll.projection.repository;
 import com.integration.test.integrationtest.domain.poll.Vote;
 import com.integration.test.integrationtest.service.vote.projection.VoteProjection;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface VoteRepository extends MongoRepository<Vote, String> {
 
-  List<VoteProjection> getVotes(String idPoll);
+  List<VoteProjection> getVotes(UUID idPoll);
 
 }
